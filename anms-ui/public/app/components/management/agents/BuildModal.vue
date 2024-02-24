11d4122ca7a547e19fd2fb3adf787aeb790457b4
Nick Brunner <njbrunner@gmail.com>
2024-02-23 19:45:15 -0500

    #55: Added build modal and logic to track selection of agents

---
+++ anms-ui/public/app/components/management/agents/BuildModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-modal id="buildModal"
+      ref="buildModal"
+      size="xl"
+      ok-only
+      @hide="closeModal"
+      title="Build">
+      <div v-if="agents">
+        <h3>Managed Agents:</h3>
+        <ul>
+          <li v-for="agent in agents">{{ agent.agent_id_string }}</li>
+        </ul>
+      </div>
+    </b-modal>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "BuildModal",
+  components: {},
+  data() {
+    return {}
+  },
+  props: {
+    showModal: {
+      type: Boolean,
+      default: false,
+    },
+    agents: {
+      type: Array,
+      default: [],
+    },
+  },
+  watch: {
+    showModal(newValue, _) {
+      const vm = this;
+      if (newValue === true) {
+        this.show();
+      }
+    }
+  },
+  computed: {},
+  methods: {
+    show() {
+      this.$refs['buildModal'].show();
+    },
+    closeModal() {
+      this.$emit("close");
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
