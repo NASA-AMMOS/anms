1ec9257f71f48cf8748e6684f229a67471e851f4
Brian Sipos <brian.sipos@jhuapl.edu>
2024-04-03 14:44:51 -0400

    Move RHEL specifics into RHEL case

--- puppet/modules/anms/manifests/docker.pp
+++ puppet/modules/anms/manifests/docker.pp
@@ -9,23 +9,25 @@ class anms::docker() {
         ensure => 'absent',
         before => Package['docker-ce'],
       }
+      package { 'yum-utils':
+        ensure => 'installed',
+      }
+      file { '/etc/yum.repos.d/docker.repo':
+        ensure => 'absent',
+      }
+      exec { 'yum-repo-docker-ce':
+        path    => $facts['path'],
+        command => 'yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo',
+        creates => '/etc/yum.repos.d/docker-ce.repo',
+        require => Package['yum-utils'],
+        before  => [Package['docker-ce'], Package['docker-buildx-plugin'], Package['docker-compose-plugin']],
+      }
     }
     default: {}
   }
 
-  package { 'yum-utils':
-    ensure  => 'installed',
-  }
-  file { '/etc/yum.repos.d/docker.repo':
-    ensure => 'absent',
-  }
-  exec { 'yum-repo-docker-ce':
-    path    => $facts['path'],
-    command => 'yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo',
-    creates => '/etc/yum.repos.d/docker-ce.repo',
-  }
   package { ['docker-ce', 'docker-ce-cli', 'containerd.io', 'docker-buildx-plugin', 'docker-compose-plugin']:
-    require => Exec['yum-repo-docker-ce'],
+    ensure => 'installed',
   }
   service { 'docker':
     require => Package['docker-ce'],
