b98510c97a25636a2f7e2bd83eec52ac8f94444b
David Linko <david.linko@jhuapl.edu>
2025-04-17 22:43:49 -0400

    updating nm_host from ion-manger to anms-amp-manager-1. still having issues submitting reports

--- anms-core/anms/shared/config.py
+++ anms-core/anms/shared/config.py
@@ -126,7 +126,7 @@ class BaseConfig(AbstractConfig):
     MQTT_USER = None
 
     # nm Settings
-    NM_HOST = "ion-manager"
+    NM_HOST = "anms-amp-manager-1"
     NM_PORT = 8089
     NM_API_BASE ="/nm/api"
 
