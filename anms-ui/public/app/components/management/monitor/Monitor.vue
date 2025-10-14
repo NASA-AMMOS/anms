8aacf97f6edda9764896b8859d006d2965e6f06f
David Linko <david.linko@jhuapl.edu>
2025-10-14 12:28:31 -0400

    reverting grafana upgrade

--- anms-ui/public/app/components/management/monitor/Monitor.vue
+++ anms-ui/public/app/components/management/monitor/Monitor.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <!-- incoming messages  -->
-    <iframe src="/grafana/d-solo/x5LAIuA7z/agents-copy?orgId=1&from=now-48h&to=now&panelId=-11" width="100%" height="250" frameborder="0"></iframe>
+    <iframe src="/grafana/d-solo/x5LAIuA7z/agents-copy?orgId=1&from=now-48h&to=now&panelId=11" width="100%" height="250" frameborder="0"></iframe>
     <!-- reports -->
-        <iframe src="/grafana/d-solo/oZ7xupanz/reports?orgId=1&from=now-48h&to=now&panelId=-123129" width="100%" height="250" frameborder="0"></iframe>
+        <iframe src="/grafana/d-solo/oZ7xupanz/reports?orgId=1&from=now-48h&to=now&panelId=123129" width="100%" height="250" frameborder="0"></iframe>
     <!-- aris -->
-    <iframe src="/grafana/d-solo/x5LAIuA7z/agents-copy?orgId=1&panelId=-6" width="100%"  height="250" frameborder="0"></iframe>
+    <iframe src="/grafana/d-solo/x5LAIuA7z/agents-copy?orgId=1&panelId=6" width="100%"  height="250" frameborder="0"></iframe>
     <!-- dashboard -->
-    <iframe src="/grafana/dashboard/new?orgId=-1" width="100%" height="120%" frameborder="0"></iframe>
+    <iframe src="/grafana/dashboard/new?orgId=1" width="100%" height="120%" frameborder="0"></iframe>
 </div>
 
 </template>
