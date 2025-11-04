2f9ef9380f42fa7acea9986544c4c9b4703a3ccb
David Linko <david.linko@jhuapl.edu>
2025-11-04 11:01:08 -0500

    updated ID for header table

--- anms-ui/public/app/components/management/agents/reports.vue
+++ anms-ui/public/app/components/management/agents/reports.vue
@@ -12,7 +12,7 @@
         v-model="selected"
         @option:selected="onReportSelect()"></v-select>
     <div v-for="(_, index) in tableItems" :key="index">
-      <b-table id="report-table" :items="tableHeaders[index]" thead-class="d-none"/>
+      <b-table id="report-header" :items="tableHeaders[index]" thead-class="d-none"/>
       <div class="scrollable-div">
         <b-table 
         bordered
